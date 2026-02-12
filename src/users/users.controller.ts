e9b1f2e981ef11c2933b03b511ec2309efcfa398
Jose Gustavo <josegustavo@MacBook-Air-de-Jose.local>
2026-02-12 17:09:05 -0600

    set secutiry in user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto, UpdateUserDto } from './users.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -8,22 +9,27 @@ export class UsersController {
     private readonly usersService: UsersService,
   ) {}
 
+  // for creating users an access_token is not required in POST /users
   @Post()
   async create(@Body() body: CreateUserDto) {
     return await this.usersService.create(body);
   }
 
+  // for consuming data from data base is required an access_token
   @Get()
+  @UseGuards(JwtAuthGuard)
   async findAll() {
     return await this.usersService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   async findOne(@Param('id') id: string) {
     return await this.usersService.findOne(id);
   }
 
   @Put(':id')
+  @UseGuards(JwtAuthGuard)
   async update(@Param('id') id: string, @Body() body: UpdateUserDto) {
     return await this.usersService.update(id, body);
   }
