8846e5627cd467317eb890a29f5454743dd4a436
Jose Gustavo <josegustavo@MacBook-Air-de-Jose.local>
2026-02-13 11:36:43 -0600

    fix possible issue with jwt_strategy

--- src/auth/jwt.strategy.spec.ts
+++ src/auth/jwt.strategy.spec.ts
@@ -1,23 +1,30 @@
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigService } from '@nestjs/config';
+import { Role } from 'src/users/user.entity';
 
-describe('JwtStrategy tests', () => {
-  let strategy: JwtStrategy;
+describe('JwtStrategy', () => {
+  let jwtStrategy: JwtStrategy;
+  let configService: ConfigService;
 
   beforeEach(() => {
-    strategy = new JwtStrategy({
-      get: jest.fn().mockReturnValue('secret'),
-    } as any)
+    process.env.JWT_SECRET = 'test_secret';
+
+    configService = {
+      get: jest.fn(),
+    } as any;
+
+    jwtStrategy = new JwtStrategy(configService);
   });
 
-  it('should validate payload', async () => {
-    const result = await strategy.validate({
-      sub: '1',
-      role: 'admin',
-    });
+  it('should be defined', () => {
+    expect(jwtStrategy).toBeDefined();
+  });
 
-    expect(result).toEqual({
-      userId: '1',
-      role: 'admin',
-    });
+  it('should validate and return user object', async () => {
+    const payload = { sub: 1, role: Role.ADMIN };
+
+    const result = await jwtStrategy.validate(payload);
+
+    expect(result).toEqual({ userId: 1, role: Role.ADMIN });
   });
 });
